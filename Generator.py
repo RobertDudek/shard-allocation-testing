ee936686bb871560dfd46f5c4f598e1e4ed8dc8c
RobertDudek <robert.dudek@student.put.poznan.pl>
2022-01-13 11:15:01 +0100

    Add homogeneity. Change phase length to number of phases

--- Generator.py
+++ Generator.py
@@ -11,12 +11,6 @@ class Generator:
     def generate_exp(self, scale=1.0, size=1000):
         return self.rng.exponential(scale=scale, size=size)
 
-    @staticmethod
-    def generate_phase(lambda1, lambda2, alpha1, alpha2):
-        a = (alpha1, alpha2)
-        m = [[lambda1, 0], [0, lambda2]]
-        raise NotImplementedError
-
     def generate_int(self, low=1, high=1000, size=1000):
         return self.rng.integers(low=low, high=high, size=size)
 
@@ -24,21 +18,34 @@ class Generator:
         return self.rng.gamma(shape=k, scale=scale, size=size)
 
     def generate_file(self, size=1000, scale_exp_low=1.0, scale_exp_high=2.0,
-                      scale_length_erlang=1.0, length_erlang_k=3, phase_length=5, file_name=None, phase_enable=False):
+                      scale_length_erlang=1.0, length_erlang_k=3, file_name=None, phases=1, homogeneity=1):
+        if phases < 1 or phases > size/2:
+            raise ValueError('The number of phases has to be between 1 and half the size')
+        if homogeneity < 0 or homogeneity > 1:
+            raise ValueError('Homogeneity needs to be between 0 and 1.')
 
         shards = self.generate_int(size=size)
-        dTS = self.generate_exp(scale=scale_exp_low, size=size)
-        dTS_low = self.generate_exp(scale=scale_exp_low, size=size//2)
-        dTS_high = self.generate_exp(scale=scale_exp_high, size=size//2 + size%2)
+        dTS = []
+        for i in range(phases):
+            for j in range(size//phases):
 
-        # maybe alternate every n items in phases will be ok?
-        if phase_enable:
-            dTS = []
-            for i in range(len(dTS_high)):
-                dTS.extend(dTS_low[i*phase_length:(i+1)*phase_length])
-                dTS.extend(dTS_high[i * phase_length:(i+1)*phase_length])
+                if self.rng.random() < homogeneity:
+                # first half prefer short task
+                    if j < (size//phases/2):
+                        dTS.extend(self.generate_exp(scale_exp_low, 1))
+                # second half prefer long task
+                    else:
+                        dTS.extend(self.generate_exp(scale_exp_high, 1))
+                else:
+                    if j < (size//phases/2):
+                        dTS.extend(self.generate_exp(scale_exp_high, 1))
+                    else:
+                        dTS.extend(self.generate_exp(scale_exp_low, 1))
+
+        # fill rest with exp_low, when {phases} doesn't divide {size}
+        dTS.extend(self.generate_exp(scale_exp_low, size % phases))
         TS = np.cumsum(dTS)
-        lengths = self.generate_erlang(k=length_erlang_k, scale=scale_length_erlang)
+        lengths = self.generate_erlang(k=length_erlang_k, scale=scale_length_erlang, size=size)
         tasks = [Task(s, dTS, TS, l) for s, dTS, TS, l in zip(shards, dTS, TS, lengths)]
         if file_name:
             with open(file_name, mode='w') as f:
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from CloudNode import CloudNode
 if __name__ == '__main__':
     gen = Generator()
     cloud_nodes = 100
-    r = gen.generate_file(file_name='Example1.txt', scale_exp_low=5.0)
+    r = gen.generate_file(file_name='Example1.txt', scale_exp_low=1.0, scale_exp_high=10.0, phases=5, homogenity=1.0)
     #r = Reader.read_file('Example1.txt')
     print(*r, sep='\n')
     print('Mean deltaTS:', r[~0].TS/len(r), 'Mean length:', sum(x.length for x in r)/len(r))
