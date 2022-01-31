31bf7c3bf24f2923423f54348078eb454e725340
RobertDudek <robert.dudek@student.put.poznan.pl>
2022-01-31 02:21:06 +0100

    Revamp input parameters for generator

--- Generator.py
+++ Generator.py
@@ -1,6 +1,6 @@
 import numpy as np
 from Task import Task
-# import butools possible library for phase distribution
+import warnings
 
 
 class Generator:
@@ -12,19 +12,36 @@ class Generator:
         return self.rng.exponential(scale=scale, size=size)
 
     def generate_int(self, low=1, high=1000, size=1000):
-        return self.rng.integers(low=low, high=high, size=size)
+        return self.rng.integers(low=low, high=high+1, size=size)
 
     def generate_erlang(self, k=3, scale=1.0, size=1000):
         return self.rng.gamma(shape=k, scale=scale, size=size)
 
-    def generate_file(self, size=1000, scale_exp_low=1.0, scale_exp_high=2.0,
-                      scale_length_erlang=1.0, length_erlang_k=3, file_name=None, phases=1, homogeneity=1):
+    def generate_file(self, load=0.50, submission_times_CV_delta=0.50, task_mean_length=100, homogeneity=1, size=10000,
+                      shards_num=1000, length_erlang_k=3, file_name=None, phases=10):
         if phases < 1 or phases > size/2:
             raise ValueError('The number of phases has to be between 1 and half the size')
         if homogeneity < 0 or homogeneity > 1:
             raise ValueError('Homogeneity needs to be between 0 and 1.')
+        if submission_times_CV_delta < 0 or submission_times_CV_delta > 1:
+            raise ValueError('Submission_times_CV_delta needs to be between 0 and 1.')
+        if load < 0 or load > 1:
+            raise ValueError('Load needs to be between 0 and 1.')
+        if task_mean_length < 0:
+            raise ValueError('Task mean length needs to be between higher than 0.')
+        if size != int(size):
+            raise ValueError('Size needs to be an integer.')
+        if shards_num != int(shards_num):
+            raise ValueError('Number of shards needs to be an integer.')
+        if length_erlang_k != int(length_erlang_k):
+            warnings.warn("By using non-integer k you are using a gamma distribution, "
+                          "the extension of the erlang distribution")
 
-        shards = self.generate_int(size=size)
+        submission_time_mean_length = task_mean_length/load
+        scale_exp_low = submission_times_CV_delta * submission_time_mean_length
+        scale_exp_high = (1+submission_times_CV_delta) * submission_time_mean_length
+        scale_length_erlang = task_mean_length/length_erlang_k
+        shards = self.generate_int(size=size, high=shards_num)
         dTS = []
         for i in range(phases):
             for j in range(size//phases):
--- main.py
+++ main.py
@@ -5,17 +5,27 @@ from ShardVector import ShardVector
 from CloudNode import CloudNode
 from ShardAllocator import ShardAllocator
 
-
-
 if __name__ == '__main__':
     gen = Generator()
     cloud_nodes = 100
-    r = gen.generate_file(file_name='Example1.txt', scale_exp_low=1.0, scale_exp_high=10.0, phases=5, homogeneity=1.0)
-    #r = Reader.read_file('Example2.txt')
-    #print(*r, sep='\n')
-    #print('Mean deltaTS:', r[~0].TS/len(r), 'Mean length:', sum(x.length for x in r)/len(r))
+    # Teraz można używać parametrów (D - Dziedzina):
+    # 1) load = obciążenie, testować ok 0.2-0.8  D=(0, 1) domyślnie 0.5
+    # 2) submission_times_CV_delta = wsp. zmienności czasów przekładania,
+    # ale w % naszych możliwości (nie bezwględnie), testować ok 0.1-0.9 D=(0, 1) domyślnie 0.5
+    # 3) homogenity = jednorodność, testować 0.5-1.0 D=(0, 1) domyślnie 1.0
+    # 4) task_mean_length = śr. dł. zadań, nie wiem po co ale można je też zmieniać,
+    # ja bym nie dawał za krótkich, domyślnie 100 D=(0, inf) (od tego i obciążenia zależy śr. dł. cz. przedkładania),
+    # (k erlanga to domyślnie 3, length_erlang_k jak chcecie zmienić rozkład)
+    # 5) phases = liczba faz, testować może <1,1/4*size> (?) D=<1, 1/2*size> (int) domyślnie 10
+    # 6) shards_num = ilość szardów, D=<1,inf) (int) domyślnie 1,000
+    # 7) size = ile wygenerować zadań, z założenia można iść w nieskończoność, D=(1,inf) (int) domyślnie 10,000 ,
+    r = gen.generate_file(file_name='Example1.txt', load=0.5, submission_times_CV_delta=0.5, homogeneity=1.0,
+                          task_mean_length=100, phases=10, shards_num=1000, size=10000)
+    # r = Reader.read_file('Example2.txt')
+    # print(*r, sep='\n')
+    print('Mean deltaTS:', r[~0].TS / len(r), 'Mean length:', sum(x.length for x in r) / len(r))
     ###
-    #100 przedziałów, 100 węzłów
+    # 100 przedziałów, 100 węzłów
     tasksAll = TasksAllocator(r, 100, cloud_nodes)
     tasksAll.find_shards_load_vectors()
     tasksAll.find_wts()
@@ -23,13 +33,12 @@ if __name__ == '__main__':
     minusNWTS = list(map(lambda x: x * -1, tasksAll.norm_wts))
     WS_vector = [0] * len(tasksAll.intervals)
     ###
-    #Umieść wektory obciążenia Wi wszystkich fragmentów danych na liście LW posortowanej ze względu na malejący moduł.
+    # Umieść wektory obciążenia Wi wszystkich fragmentów danych na liście LW posortowanej ze względu na malejący moduł.
     shardVectors = []
     for k, v in tasksAll.shards_load_vect.items():
         shardVec = ShardVector(k, v, sum(v))
         shardVectors.append(shardVec)
-    #shardVectors.sort(key=lambda x: x.sum, reverse=True)
-
+    # shardVectors.sort(key=lambda x: x.sum, reverse=True)
 
     print("-----------RoundRobin-----------")
     rr = ShardAllocator(shardVectors, cloud_nodes, r, tasksAll.norm_wts, WS_vector, minusNWTS)
